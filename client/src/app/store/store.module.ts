45ae845b29e44f3f7f999ad5d39267213c845bb7
Gabriel <drozd.gj@gmail.com>
2021-11-29 18:11:35 +0100

    Added store products pattern with filters option on side

--- client/src/app/store/store.module.ts
+++ client/src/app/store/store.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    StoreComponent
+    StoreComponent,
+    ProductItemComponent
   ],
   exports: [
     StoreComponent

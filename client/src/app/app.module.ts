991d77655a45b6bdfadd86c3ddf0393dc6af2de6
Gabriel <drozd.gj@gmail.com>
2021-12-04 12:27:37 +0100

    New module and components added to take care of routing in application
    Routing added. Clicking on navigation bar items now will take user to specific place in store.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
-import { CoreModule } from './core/core.module';
+import {CoreModule} from './core/core.module';
 import {StoreModule} from "./store/store.module";
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    CoreModule,
-    StoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        CoreModule,
+        StoreModule,
+        HomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/store/store.module.ts
+++ client/src/app/store/store.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StoreComponent } from './store.component';
-import { ProductItemComponent } from './product-item/product-item.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StoreComponent} from './store.component';
+import {ProductItemComponent} from './product-item/product-item.component';
 import {SharedModule} from "../shared/shared.module";
-
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  declarations: [
-    StoreComponent,
-    ProductItemComponent
-  ],
-  exports: [
-    StoreComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+    declarations: [
+        StoreComponent,
+        ProductItemComponent,
+        ProductDetailsComponent
+    ],
+    exports: [
+        StoreComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        RouterModule
+    ]
 })
-export class StoreModule { }
+export class StoreModule {
+}

9b0db3da5209b61167adb80653470ffd72bbeac7
Gabriel <drozd.gj@gmail.com>
2021-12-08 12:23:46 +0100

    Added Toastr Popup Notifications. Added Server error handling page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import {HomeComponent} from './home/home.component';
+import {NotFoundComponent} from "./core/not-found/not-found.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'test-error', component: TestErrorComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path: 'not-found', component: NotFoundComponent},
     {path: 'store', loadChildren: () => import('./store/store.module').then(mod => mod.StoreModule)},
     {path: '**', redirectTo: '', pathMatch: 'full'},
 ];

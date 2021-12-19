28be5a1e85215446fd3f02ab78538046f242f7ec
Gabriel <drozd.gj@gmail.com>
2021-12-19 03:11:58 +0100

    Added register form. Added validation for register form
    Validation for register form to prevent unnecessary api calls

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AccountService} from '../account.service';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
     selector: 'app-login',
@@ -10,11 +10,13 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
+    returnUrl: string;
 
-    constructor(private accountService: AccountService, private router: Router) {
+    constructor(private accountService: AccountService, private router: Router, private activatedRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
+        this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/store';
         this.createLoginForm();
     }
 
@@ -27,7 +29,7 @@ export class LoginComponent implements OnInit {
 
     onSubmit() {
         this.accountService.login(this.loginForm.value).subscribe(() => {
-            this.router.navigateByUrl('/store').then(() => {
+            this.router.navigateByUrl(this.returnUrl).then(() => {
                 console.log('logged in');
             });
         }, error => {
